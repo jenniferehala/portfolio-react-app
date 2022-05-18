1d30b986b05cedb666bcd89f1f6113f891362d68
jehala <jenniferehala@github.com>
2022-05-18 11:08:46 -0400

    initial commit

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+    return (
+        <div name='home' className='w-full h-screen bg-[#0a192f]'>
+            {/* Container */}
+            <div className='maz-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
