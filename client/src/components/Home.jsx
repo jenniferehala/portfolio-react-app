caa02df57cdb30438adc44f442234b9cea5496f3
jehala <jenniferehala@github.com>
2022-05-19 17:20:13 -0400

    commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
+import myResume from '../../src/resume.pdf'
 
 const Home = () => {
     return (
@@ -10,13 +12,26 @@ const Home = () => {
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Jennifer Ehala</h1>
                 <h2 className="text-4xl sm:text-6xl font-bold text-[#8892b0]">I'm a Full-Stack Developer </h2>
                 <p className="text-[#8892b0] py-4 max-w-[700px]">specializing in building and designing exceptional digital experiences. Currently, I'm focused on building responsice full-stack web applications.</p>
-                <div>
+
+                <div className='flex justify-start gap-10'>
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                        View Work
+                        <Link to='work' smooth={true} duration={500}>
+                            View Work
+                        </Link>
                         <span className='group-hover:rotate-90 duration-300 '>
                             <HiArrowNarrowRight className='ml-3' />
+
                         </span>
                     </button>
+                    <a href={myResume} target="_blank">
+                        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                            View Resume
+                        </button>
+                    </a>
+
+                </div>
+                <div>
+
                 </div>
             </div>
         </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,10 +8,13 @@ import {
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/mono2.png'
+import { Link } from 'react-scroll'
+import myResume from '../../src/resume.pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav)
+
 
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
@@ -21,11 +24,31 @@ const Navbar = () => {
 
             {/* menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to='home' smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='about' smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to='skills' smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to='work' smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to='contact' smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
 
@@ -36,38 +59,72 @@ const Navbar = () => {
             </div >
 
             {/* mobile menu */}
-            <ul className={!nav ? 'hidden' : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Skills</li>
-                <li className='py-6 text-4xl'>Work</li>
-                <li className='py-6 text-4xl'>Contact</li>
+
+            <ul className={
+                !nav
+                    ? 'hidden'
+                    : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            }
+            >
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li className='py-6 text-4xl'>
+                    <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
-            {/* social icons */}
+            {/* Social icons */}
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                            href="/">
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href="https://www.linkedin.com/in/jenniferehala/"
+                            target="_blank">
                             LinkedIn <FaLinkedin size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                            href="/">
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href="https://github.com/jenniferehala"
+                            target="_blank">
                             Github <FaGithub size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                            href="/">
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href="mailto:jennifer.ehala@gmail.com"
+                            target="_blank">
                             Email <HiOutlineMail size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                            href="/">
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href={myResume}
+                            target="_blank">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
